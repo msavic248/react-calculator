17bea1b8bdbb45dd53b5594c56705f5fe4b0ec89
Milos Savic <msavic248@yahoo.com>
2022-10-07 17:54:56 +0200

    Added calc functionality

---
+++ src/DigitButton.jsx
@@ -0,0 +1,7 @@
+import {ACTIONS} from './App.jsx';
+
+export default function DigitButton({dispatch, digit}) {
+  return (
+    <button onClick={() => dispatch({type: ACTIONS.ADD_DIGIT, payload: {digit}})}>{digit}</button>
+  )
+}
